633b38096f1d8de10619db51721f0b896f35bb62
simo07d7PG <simo07d7.pg@gmail.com>
2026-03-19 15:41:33 +0900

    [Feat] 애니메이션 및 점프 추가

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,30 +4,45 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     private Vector2 moveInput;
-    public float moveSpeed;
+    public float moveSpeed = 10.0f;
+    public float jumpModifier = 10.0f;
+    private Rigidbody2D rb;
+    private Animator ani;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        ani = GetComponent<Animator>();
+        if(ani == null) { Debug.Log("fail to get component"); }
+        ani.SetBool("move", false);
+    }
     public void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void OnJump(InputValue value)
     {
-        
+        if(value.isPressed)
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpModifier);
     }
-
     // Update is called once per frame
     void Update()
     {
-        if(moveInput.x > 0)
+        if (moveInput.x > 0)
         {
             transform.localScale = new Vector3(1, 1, 1);
             Debug.Log("0");
         }
         else if (moveInput.x < 0)
         {
-            transform.localScale = new Vector3(-1 , 1, 1);
+            transform.localScale = new Vector3(-1, 1, 1);
             Debug.Log("1");
         }
+        if (moveInput.magnitude > 0) { ani.SetBool("move", true); }
+        else {
+            ani.SetBool("move", false);
+        }
+
         transform.Translate(Vector3.right * moveInput.x * Time.deltaTime * moveSpeed);
     }
 }
